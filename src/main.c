f588cdf5381b80d912c8dab140143d38bec14f80
Brett R. Toomey <brert@orsted.com>
2022-05-15 21:03:22 +0200

    Tabs to 4 spaces

--- src/main.c
+++ src/main.c
@@ -24,19 +24,19 @@ typedef double f64;
 typedef BENCHMARK(Benchmark);
 
 typedef enum Flags {
-	Flags_None = 0,
-	Flags_ARM  = (1 << 0),
-	Flags_x86  = (1 << 1),
+    Flags_None = 0,
+    Flags_ARM  = (1 << 0),
+    Flags_x86  = (1 << 1),
 } Flags;
 
 #define BENCH_WARMUP_COUNT 10
 #define BENCH_RESULT_COUNT 10
 
 typedef struct Benchmark_Entry {
-	const char *name;
-	Benchmark *benchmark;
-	u32 flags;
-	f64 results[BENCH_RESULT_COUNT];
+    const char *name;
+    Benchmark *benchmark;
+    u32 flags;
+    f64 results[BENCH_RESULT_COUNT];
 } Benchmark_Entry;
 
 typedef __m256i u64x4;
@@ -48,98 +48,97 @@ typedef __m256i u64x4;
 
 BENCHMARK(bench_memset)
 {
-	memset(results, 0, count * sizeof(*results));
+    memset(results, 0, count * sizeof(*results));
 }
 
 BENCHMARK(bench_rand_u64)
 {
-	Rng rng = new_rng();
+    Rng rng = new_rng();
 
-	for (int i = 0; i < count; i++) {
-		u64 u = rand_u64(&rng);
-		results[i] = *(f64 *)&u; 
-	}
+    for (int i = 0; i < count; i++) {
+        u64 u = rand_u64(&rng);
+        results[i] = *(f64 *)&u; 
+    }
 }
 
 BENCHMARK(bench_rand_u64x4)
 {
-	Rng_x4 rng = new_rng_x4();
+    Rng_x4 rng = new_rng_x4();
 
-	for (int i = 0; i < count; i += 4) {
-
-		u64x4 *p = (u64x4 *) (results + i);
-		u64x4 u = rand_u64x4(&rng);
-		_mm256_store_si256(p, u);
-	}
+    for (int i = 0; i < count; i += 4) {
+        u64x4 *p = (u64x4 *) (results + i);
+        u64x4 u = rand_u64x4(&rng);
+        _mm256_store_si256(p, u);
+    }
 }
 
 BENCHMARK(bench_rand_f64)
 {
-	Rng rng = new_rng();
+    Rng rng = new_rng();
 
-	for (int i = 0; i < count; i++) {
-		results[i] = rand_f64(&rng);
-	}
+    for (int i = 0; i < count; i++) {
+        results[i] = rand_f64(&rng);
+    }
 }
 
 Benchmark_Entry entries[] = {
-	{ "Memset",                   bench_memset,             Flags_ARM | Flags_x86 },
-	{ "Rand u64",                 bench_rand_u64,           Flags_ARM | Flags_x86 },
-	{ "Rand u64x4",               bench_rand_u64x4,                     Flags_x86 },
-	{ "Rand f64",                 bench_rand_f64,           Flags_ARM | Flags_x86 },
-	{ "Inverse Normal CDF",       bench_inverse_normal_cdf, Flags_ARM | Flags_x86 },
-	{ "Boxmuller",                bench_boxmuller,          Flags_ARM | Flags_x86 },
-	{ "Central Limit (4 draws)",  bench_central_limit_4,    Flags_ARM | Flags_x86 },
-	{ "Central Limit (8 draws)",  bench_central_limit_8,    Flags_ARM | Flags_x86 },
-	{ "Central Limit (16 draws)", bench_central_limit_16,   Flags_ARM | Flags_x86 },
+    { "Memset",                   bench_memset,             Flags_ARM | Flags_x86 },
+    { "Rand u64",                 bench_rand_u64,           Flags_ARM | Flags_x86 },
+    { "Rand u64x4",               bench_rand_u64x4,                     Flags_x86 },
+    { "Rand f64",                 bench_rand_f64,           Flags_ARM | Flags_x86 },
+    { "Inverse Normal CDF",       bench_inverse_normal_cdf, Flags_ARM | Flags_x86 },
+    { "Boxmuller",                bench_boxmuller,          Flags_ARM | Flags_x86 },
+    { "Central Limit (4 draws)",  bench_central_limit_4,    Flags_ARM | Flags_x86 },
+    { "Central Limit (8 draws)",  bench_central_limit_8,    Flags_ARM | Flags_x86 },
+    { "Central Limit (16 draws)", bench_central_limit_16,   Flags_ARM | Flags_x86 },
 };
 
 void run_benchmarks( f64 *samples, i32 sample_count )
 {
-	i32 count = sizeof(entries) / sizeof(entries[0]);
-	for (i32 i = 0; i < count; i++) {
-		Benchmark_Entry *entry = entries + i;
+    i32 count = sizeof(entries) / sizeof(entries[0]);
+    for (i32 i = 0; i < count; i++) {
+        Benchmark_Entry *entry = entries + i;
 
-		for (int j = 0; j < BENCH_WARMUP_COUNT; j++) {
-			entry->benchmark(samples, sample_count);
-		}
+        for (int j = 0; j < BENCH_WARMUP_COUNT; j++) {
+            entry->benchmark(samples, sample_count);
+        }
 
-		f64 sum = 0.0;
-		
-		for (int j = 0; j < BENCH_RESULT_COUNT; j++) {
-			u64 cycles_start = get_cycles();
-			entry->benchmark(samples, sample_count);
-			u64 cycles_end = get_cycles();
+        f64 sum = 0.0;
+        
+        for (int j = 0; j < BENCH_RESULT_COUNT; j++) {
+            u64 cycles_start = get_cycles();
+            entry->benchmark(samples, sample_count);
+            u64 cycles_end = get_cycles();
 
-			f64 cycles = cycles_end - cycles_start;
-			f64 cycles_per_sample = cycles / (f64)(sample_count);
+            f64 cycles = cycles_end - cycles_start;
+            f64 cycles_per_sample = cycles / (f64)(sample_count);
 
-			entry->results[j] = cycles_per_sample;
-			sum += cycles_per_sample;
-		}
+            entry->results[j] = cycles_per_sample;
+            sum += cycles_per_sample;
+        }
 
-	#if 0
-		u64 cpu_clock_speed = 3200000000; /* M1 clock speed is 3,2Ghz */
-	#else
-		u64 cpu_clock_speed = 2400000000; /* My i9 clock speed is 2,4Ghz */
-	#endif
+    #if 0
+        u64 cpu_clock_speed = 3200000000; /* M1 clock speed is 3,2Ghz */
+    #else
+        u64 cpu_clock_speed = 2400000000; /* My i9 clock speed is 2,4Ghz */
+    #endif
 
-		f64 avg_cycles_per_sample = sum / BENCH_RESULT_COUNT;
-		f64 samples_per_second = cpu_clock_speed / avg_cycles_per_sample;
-		f64 bytes_per_second = samples_per_second * sizeof(*samples);
+        f64 avg_cycles_per_sample = sum / BENCH_RESULT_COUNT;
+        f64 samples_per_second = cpu_clock_speed / avg_cycles_per_sample;
+        f64 bytes_per_second = samples_per_second * sizeof(*samples);
 
-		printf("%s:\n"
-			   	"  Average cycles per sample: %.2lf, %.2lf/bps\n",
-				entry->name, avg_cycles_per_sample, bytes_per_second);
-	}
+        printf("%s:\n"
+                "  Average cycles per sample: %.2lf, %.2lf/bps\n",
+                entry->name, avg_cycles_per_sample, bytes_per_second);
+    }
 }
 
 int main( void )
 {
-	i32 sample_count = 10e6;
-	f64 *sample_buffer = aligned_alloc(32, sample_count * sizeof(*sample_buffer));
+    i32 sample_count = 10e6;
+    f64 *sample_buffer = aligned_alloc(32, sample_count * sizeof(*sample_buffer));
 
-	run_benchmarks(sample_buffer, sample_count);
+    run_benchmarks(sample_buffer, sample_count);
 
-	return 0;
+    return 0;
 }
--- src/rng.c
+++ src/rng.c
@@ -1,79 +1,79 @@
 // The RNG used is Xorshift128++
 
 typedef struct Rng {
-	u64 s0;
-	u64 s1;
+    u64 s0;
+    u64 s1;
 } Rng;
 
 typedef struct __attribute__((aligned(32))) Rng_x4 {
-	u64x4 s0;
-	u64x4 s1;
+    u64x4 s0;
+    u64x4 s1;
 } Rng_x4;
 
 Rng new_rng( void )
 {
-	Rng result = {0};
-	result.s0 = 0xababab;
-	result.s1 = 0xbababa;
-	return result;
+    Rng result = {0};
+    result.s0 = 0xababab;
+    result.s1 = 0xbababa;
+    return result;
 }
 
 Rng_x4 new_rng_x4( void )
 {
-	Rng_x4 result = {0};
-	result.s0 = _mm256_set1_epi64x(0xababab);
-	result.s1 = _mm256_set1_epi64x(0xbababa);
-	return result;
+    Rng_x4 result = {0};
+    result.s0 = _mm256_set1_epi64x(0xababab);
+    result.s1 = _mm256_set1_epi64x(0xbababa);
+    return result;
 }
 
 u64 rand_u64( Rng *state )
 {
-	u64 s0 = state->s0;
-	u64 s1 = state->s1;
+    u64 s0 = state->s0;
+    u64 s1 = state->s1;
 
-	u64 r = s0 + s1;
+    u64 r = s0 + s1;
 
-	s1 ^= s1 << 23;
-	state->s1 = s1 ^ s0 ^ (s1 >> 18) ^ (s0 >> 5);
+    s1 ^= s1 << 23;
+    state->s1 = s1 ^ s0 ^ (s1 >> 18) ^ (s0 >> 5);
 
-	return r;
+    return r;
 }
 
 u64x4 rand_u64x4( Rng_x4 *state )
 {
-	u64x4 s0 = state->s0;
-	u64x4 s1 = state->s1;
+    u64x4 s0 = state->s0;
+    u64x4 s1 = state->s1;
 
-	u64x4 r = _mm256_add_epi64(s0, s1);
+    u64x4 r = _mm256_add_epi64(s0, s1);
 
-	// s1 ^= s1 << 23;
-	s1 = _mm256_xor_si256(s1, _mm256_slli_epi64(s1, 23));
+    // s1 ^= s1 << 23;
+    s1 = _mm256_xor_si256(s1, _mm256_slli_epi64(s1, 23));
 
-	// s1 = s1 ^ s0 ^ (s1 >> 18) ^ (s0 >> 5);
-	s1 = _mm256_xor_si256(
-		s1,
-		_mm256_xor_si256(
-			s0,
-			_mm256_xor_si256(
-				_mm256_srli_epi64(s1, 18), 
-				_mm256_srli_epi64(s0, 5))));
+    // s1 = s1 ^ s0 ^ (s1 >> 18) ^ (s0 >> 5);
+    s1 = _mm256_xor_si256(
+        s1,
+        _mm256_xor_si256(
+            s0,
+            _mm256_xor_si256(
+                _mm256_srli_epi64(s1, 18), 
+                _mm256_srli_epi64(s0, 5))));
 
-	state->s1 = s1;
+    state->s1 = s1;
 
-	return r;
+    return r;
 }
 
 f64 rand_f64( Rng *state )
 {
-	u64 s0 = state->s0;
-	u64 s1 = state->s1;
+    u64 s0 = state->s0;
+    u64 s1 = state->s1;
 
-	u64 r = s0 + s1;
+    u64 r = s0 + s1;
 
-	s1 ^= s1 << 23;
-	state->s1 = s1 ^ s0 ^ (s1 >> 18) ^ (s0 >> 5);
+    s1 ^= s1 << 23;
+    state->s1 = s1 ^ s0 ^ (s1 >> 18) ^ (s0 >> 5);
 
-	const union { u64 i; f64 d; }
+    const union { u64 i; f64 d; }
     u = { .i = UINT64_C(0x3FF) << 52 | r >> 12 };
     return u.d - 1.0;
 }
